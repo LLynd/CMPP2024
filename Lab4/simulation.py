5928527dd6503b2937634fbf527ad9ae821399b6
lniedzwiedzki <lukasz.niedzwiedzki@taxusit.pl>
2024-03-20 23:18:49 +0100

    Lab4 init

---
+++ Lab4/simulation.py
@@ -0,0 +1,10 @@
+import os
+import numpy as np
+from random import randint
+import copy
+from datetime import datetime
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.colors import ListedColormap
+plt.rcParams['animation.ffmpeg_path'] = r'/usr/bin/ffmpeg'
